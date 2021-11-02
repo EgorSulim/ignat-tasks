05bac3db6428695d6a171324cbbee2867a824e94
EgorSulim <87086875+EgorSulim@users.noreply.github.com>
2021-11-02 13:32:45 +0300

    'hw5'

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import style from '../h5/HW5.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={style.header} >
+            <div className={style.links}><NavLink to={'/pre-junior'} activeClassName={style.active}> Jun </NavLink></div>
+            <div className={style.links}><NavLink to={'/error404'}activeClassName={style.active}> Jun+ </NavLink></div>
+            <div>menu</div>
         </div>
     )
 }
